33e816e34954f4c5f835dab1b2868cd9f16035ae
kavyansh18 <kavyanshkumar18@gmail.com>
2024-09-18 04:09:13 +0530

    fix:hash-router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Donors from "./Pages/Donors";
 import Register from "./Pages/Register";
